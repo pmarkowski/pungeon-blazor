5ee090244eebe181796dbf471cae81257e127c6e
Philip Markowski <philipmarkowski642@gmail.com>
2020-03-22 22:02:38 -0400

    Beef up Room tests

--- Pungeon.Web.Tests/Dungeons/RoomTests.cs
+++ Pungeon.Web.Tests/Dungeons/RoomTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Pungeon.Web.Dungeons;
 using Xunit;
 
@@ -5,6 +6,67 @@ namespace Pungeon.Web.Tests.Dungeons
 {
     public class RoomTests
     {
+        [Fact]
+        public void GetHeight_OverlappingSpaces_ReturnsLargestHeight()
+        {
+            RelativePosition zeroPosition = new RelativePosition(0, 0);
+            Room room = new Room()
+            {
+                Spaces = new List<Space>
+                {
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(2, 3)
+                    },
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(3, 4)
+                    },
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(4, 5)
+                    }
+                }
+            };
+
+            int height = room.GetHeight();
+
+            Assert.Equal(5, height);
+        }
+
+        [Fact]
+        public void GetHeight_OffsetSpaces_ReturnsLargestSumOfYAndHeight()
+        {
+            Room room = new Room()
+            {
+                Spaces = new List<Space>
+                {
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(8, 9),
+                        Size = new Size(2, 3)
+                    },
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(4, 5),
+                        Size = new Size(3, 4)
+                    },
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(0, 0),
+                        Size = new Size(4, 5)
+                    }
+                }
+            };
+
+            int height = room.GetHeight();
+
+            Assert.Equal(9 + 3, height);
+        }
+
         [Fact]
         public void GetHeight_NoSpaces_ReturnsZero()
         {
@@ -15,15 +77,75 @@ namespace Pungeon.Web.Tests.Dungeons
             Assert.Equal(0, height);
         }
 
+        [Fact]
+        public void GetWidth_OverlappingSpaces_ReturnsLargestWidth()
+        {
+            RelativePosition zeroPosition = new RelativePosition(0, 0);
+            Room room = new Room()
+            {
+                Spaces = new List<Space>
+                {
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(2, 3)
+                    },
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(3, 4)
+                    },
+                    new Space
+                    {
+                        RelativePosition = zeroPosition,
+                        Size = new Size(4, 5)
+                    }
+                }
+            };
+
+            int width = room.GetWidth();
+
+            Assert.Equal(4, width);
+        }
+
+        [Fact]
+        public void GetWidth_OffsetSpaces_ReturnsLargestSumOfXAndWidth()
+        {
+            Room room = new Room()
+            {
+                Spaces = new List<Space>
+                {
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(8, 9),
+                        Size = new Size(2, 3)
+                    },
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(4, 5),
+                        Size = new Size(3, 4)
+                    },
+                    new Space
+                    {
+                        RelativePosition = new RelativePosition(0, 0),
+                        Size = new Size(4, 5)
+                    }
+                }
+            };
+
+            int width = room.GetWidth();
+
+            Assert.Equal(8 + 2, width);
+        }
         
         [Fact]
         public void GetWidth_NoSpaces_ReturnsZero()
         {
             Room room = new Room();
 
-            int height = room.GetWidth();
+            int width = room.GetWidth();
 
-            Assert.Equal(0, height);
+            Assert.Equal(0, width);
         }
     }
 }
