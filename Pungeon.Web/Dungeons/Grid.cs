999f06c9462eccfe0003ea4b4a0b03ce476cf194
Philip Markowski <philipmarkowski642@gmail.com>
2020-04-10 11:47:04 -0400

    Add a minimum size to dungeon and to the grid

--- Pungeon.Web/Dungeons/Grid.cs
+++ Pungeon.Web/Dungeons/Grid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,11 +6,19 @@ namespace Pungeon.Web.Dungeons
 {
     public class Grid
     {
+        private Size _minimumSize;
         private Dictionary<(int x, int y), char> _grid;
 
         public Grid()
         {
             _grid = new Dictionary<(int x, int y), char>();
+            _minimumSize = new Size(0, 0);
+        }
+
+        public Grid(Size minimumSize)
+        {
+            _grid = new Dictionary<(int x, int y), char>();
+            _minimumSize = minimumSize;
         }
 
         public char this[int x, int y]
@@ -29,9 +38,9 @@ namespace Pungeon.Web.Dungeons
             }
         }
 
-        public int GetMinimumY() => _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.y);
-        public int GetMaximumY() => _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.y);
-        public int GetMinimumX() => _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.x);
-        public int GetMaximumX() => _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.x);
+        public int GetMinimumY() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.y));
+        public int GetMaximumY() => Math.Max(_minimumSize.Height, _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.y));
+        public int GetMinimumX() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.x));
+        public int GetMaximumX() => Math.Max(_minimumSize.Width, _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.x));
     }
 }
