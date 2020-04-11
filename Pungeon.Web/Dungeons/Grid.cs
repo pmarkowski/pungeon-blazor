a6fb2bbbdaf391a69421456d7d6acce84850055e
Philip Markowski <philipmarkowski642@gmail.com>
2020-04-11 12:54:14 -0400

    Make use of Tile class in Grid

--- Pungeon.Web/Dungeons/Grid.cs
+++ Pungeon.Web/Dungeons/Grid.cs
@@ -7,27 +7,30 @@ namespace Pungeon.Web.Dungeons
     public class Grid
     {
         private Size _minimumSize;
-        private Dictionary<(int x, int y), char> _grid;
+        private Dictionary<(int x, int y), Tile> _grid;
 
         public Grid()
         {
-            _grid = new Dictionary<(int x, int y), char>();
+            _grid = new Dictionary<(int x, int y), Tile>();
             _minimumSize = new Size(0, 0);
         }
 
         public Grid(Size minimumSize)
         {
-            _grid = new Dictionary<(int x, int y), char>();
+            _grid = new Dictionary<(int x, int y), Tile>();
             _minimumSize = minimumSize;
         }
 
-        public char this[int x, int y]
+        public Tile this[int x, int y]
         {
             get
             {
                 if (!_grid.ContainsKey((x ,y)))
                 {
-                    _grid[(x, y)] = '#';
+                    _grid[(x, y)] = new Tile
+                    {
+                        Character = '#'
+                    };
                 }
 
                 return _grid[(x, y)];
@@ -38,9 +41,9 @@ namespace Pungeon.Web.Dungeons
             }
         }
 
-        public int GetMinimumY() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.y));
-        public int GetMaximumY() => Math.Max(_minimumSize.Height, _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.y));
-        public int GetMinimumX() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord] != '#').Min(coord => coord.x));
-        public int GetMaximumX() => Math.Max(_minimumSize.Width, _grid.Keys.Where(coord => _grid[coord] != '#').Max(coord => coord.x));
+        public int GetMinimumY() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord].Character != '#').Min(coord => coord.y));
+        public int GetMaximumY() => Math.Max(_minimumSize.Height, _grid.Keys.Where(coord => _grid[coord].Character != '#').Max(coord => coord.y));
+        public int GetMinimumX() => Math.Min(0, _grid.Keys.Where(coord => _grid[coord].Character != '#').Min(coord => coord.x));
+        public int GetMaximumX() => Math.Max(_minimumSize.Width, _grid.Keys.Where(coord => _grid[coord].Character != '#').Max(coord => coord.x));
     }
 }
