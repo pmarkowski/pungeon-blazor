358d0e11fafcba2299ef336cb4cbe86098f92cf6
Philip Markowski <philipmarkowski642@gmail.com>
2020-05-18 15:46:13 -0400

    Update version of react version

---
+++ Pungeon.Web/wwwroot/react-build/precache-manifest.df68f6f34146e65451486e44cf5c663b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d374abd58b3c87ce414aedd1ff0d4873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b8ec6c579e55a07f7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26a6c23cca96dbb6cd83",
+    "url": "/static/js/2.4b05e7e5.chunk.js"
+  },
+  {
+    "revision": "b69c9f1b06a7ecd72dcd3bf8a085ecb7",
+    "url": "/static/js/2.4b05e7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b8ec6c579e55a07f7f",
+    "url": "/static/js/main.5f755861.chunk.js"
+  },
+  {
+    "revision": "35aab4ee11009491f5e3",
+    "url": "/static/js/runtime-main.e8e85e40.js"
+  }
+]);
